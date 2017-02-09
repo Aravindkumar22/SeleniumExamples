7be273a2c7e8d1925bbe5d6163c692cf4e7acdeb
Matthew Wilton <matthewrwilton@gmail.com>
2017-02-09 22:09:36 +1000

    Refactor for additional examples.

--- SeleniumExamples/Scaffolding.cs
+++ SeleniumExamples/Scaffolding.cs
@@ -1,31 +1,27 @@
 ﻿using System.IO;
 using System.Reflection;
-using OpenQA.Selenium.Chrome;
-using Xunit;
 
 namespace SeleniumExamples
 {
-    public class WebDriverExamples
+    static class Scaffolding
     {
-        [Fact]
-        public void Open_Google_Chrome()
+        public static string DriverDirectory
         {
-            var binDirectory = GetBinDirectory();
-            var dependenciesDirectory = Path.Combine(binDirectory, "Dependencies");
-            using (var driver = new ChromeDriver(dependenciesDirectory))
+            get
             {
-                driver.Navigate().GoToUrl("https://www.google.com.au");
+                var binDirectory = GetBinDirectory();
+                return Path.Combine(binDirectory, "Dependencies");
             }
         }
 
-        private string GetBinDirectory()
+        private static string GetBinDirectory()
         {
             var testAssemblyDirectory = Path.GetDirectoryName(Assembly.GetAssembly(typeof(WebDriverExamples)).CodeBase);
             return TrimSchemeFromUri(testAssemblyDirectory);
         }
 
         // From Marc Gravell's answer http://stackoverflow.com/a/4517321
-        private string TrimSchemeFromUri(string uri)
+        private static string TrimSchemeFromUri(string uri)
         {
             int schemeEnd = uri.IndexOf(":");
             return uri.Substring(schemeEnd + 1)
---
+++ SeleniumExamples/_1_Bare_Minimum_Web_Driver_Example.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SeleniumExamples
+{
+    public class _1_Bare_Minimum_Web_Driver_Example
+    {
+        /* 
+         * This is a bare minimum example of using Selenium WebDriver in C#.
+         * All that was required for this to run is
+         *  - installing the Selenium.WebDriver nuget
+         *  - installing the xunit and a xunit.runner (e.g. xunit.runners.visualstudio) nugets
+         *  - downloading the Chrome driver - https://chromedriver.storage.googleapis.com/index.html
+         */
+
+        [Fact]
+        public void Open_Google_Chrome()
+        {
+            using (var driver = new ChromeDriver(Scaffolding.DriverDirectory))
+            {
+                driver.Navigate().GoToUrl("https://www.google.com.au");
+            }
+        }
+    }
+}
