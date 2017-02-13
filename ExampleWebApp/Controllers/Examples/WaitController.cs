171bd2ffd7a4739042dc4b6c4760065ae02f4178
Matthew Wilton <matthewrwilton@gmail.com>
2017-02-13 21:14:36 +1000

    Add a page for testing wait code against.

---
+++ ExampleWebApp/Controllers/Examples/WaitController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ExampleWebApp.Controllers.Examples
+{
+    public class ExamplesController : Controller
+    {
+        // GET: Wait
+        public ActionResult Wait()
+        {
+            return View("Wait");
+        }
+    }
+}
