3d4d3d08b6c66f13e5087a1b8f87bb8424a052ea
Matthew Wilton <matthewrwilton@gmail.com>
2017-02-15 23:45:22 +1000

    Basic wait examples.

---
+++ SeleniumExamples/Scaffolding/ChromeDriverFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumExamples.Scaffolding
+{
+    /// <summary>
+    /// Creates a ChromeDriver instance for use in Selenium tests.
+    /// This is an xUnit fixture class. Refer to https://xunit.github.io/docs/shared-context.html.
+    /// </summary>
+    public class ChromeDriverFixture : IDisposable
+    {
+        public ChromeDriver Driver { get; }
+
+        public ChromeDriverFixture()
+        {
+            Driver = new ChromeDriver(ProjectDirectories.GetDriversDirectory());
+        }
+
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                Driver.Dispose();
+            }
+        }
+    }
+}
---
+++ SeleniumExamples/Scaffolding/SeleniumTestCollection.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace SeleniumExamples.Scaffolding
+{
+    /// <summary>
+    /// This is an xUnit test collection that starts the example web app and creates a ChromeDriver instance.
+    /// Refer to https://xunit.github.io/docs/shared-context.html.
+    /// </summary>
+    [CollectionDefinition(nameof(SeleniumTestCollection))]
+    public class SeleniumTestCollection : ICollectionFixture<WebAppFixture>, ICollectionFixture<ChromeDriverFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
